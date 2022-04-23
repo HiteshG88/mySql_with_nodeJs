d64c7df5a9019895a4ad13e733146c7c5bef06fc
Hitesh <hitesh@pop-os.localdomain>
2022-04-23 18:27:46 +0530

    :hammer: added: connection to mysql

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require("express");
-const mySql = require("mysql");
+const mySql = require("mysql2");
+
+// create a mySql connection
+const db = mySql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("mySql Connected...");
+});
 
 const app = express();
 
+app.get("/createDb", (req, res) => {
+    let sql = "CREATE DATABASE mySql_with_nodeJs";
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send("database created...");
+    });
+});
+
 PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`server running on ${PORT}`);
+    console.log(`server running on ${PORT}`);
 });
